55d482421130fbea0de34d161375ea159cb0f7ff
donrusthol <roope.rustholkarhu@gmail.com>
2024-02-09 10:56:59 +0200

    separated database uri from app.py, created gitignore

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dburi import db_uri
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(app)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key = true)
+    description = db.Column(db.String(100), nullable = false)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hey!'
 
 if __name__ == '__main__':
     app.run()
---
+++ dburi.py
@@ -0,0 +1,3 @@
+# replace YOURPASSWORD with your password used to log into pgAdmin
+# if you have a different username than "postgres", replace the second "postgres" with your username
+dburi = 'postgres://postgres:YOURPASSWORD@localhost/movehel'
